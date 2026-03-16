de7a088f3798f95383fc9cf94181bd0e94b1de91
cho byeong seon <akscnl6521@gmail.com>
2026-03-17 08:54:35 +0900

    add adsense code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4022588219132458"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
